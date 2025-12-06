64b28dce10875ad3261fa63a9e19c7626f80ff53
SrVariable <rojohnibana@gmail.com>
2025-12-06 01:57:03 +0100

    fix(db): Fix id issue
    When adding users, the municipality id can be repeated, so I added
    another field ineId which stores the real ID, and keeped an internal id.

--- db/province.sql
+++ db/province.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS province (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ineId INT NOT NULL,
     autonomousCommunityId INT NOT NULL,
     name VARCHAR(100),
 
-    PRIMARY KEY(id, autonomousCommunityId),
+    UNIQUE(ineId, autonomousCommunityId),
     CONSTRAINT FK_PROVINCE_autonomousCommunityId FOREIGN KEY(autonomousCommunityId) REFERENCES autonomousCommunity(id)
 );
