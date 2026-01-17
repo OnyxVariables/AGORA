fab4e144e0b6db9224e63c55f6bf84f55d741d15
Oliver <olivergamboamesa0850@gmail.com>
2026-01-17 22:46:19 +0100

    Solucion al fallo del anterior commit (habia conflicto porque se generaban un certificado local y otro con let's encrypt, se me olvido quitar el local)

--- docker/nginx/setup.sh
+++ docker/nginx/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-DOMAIN="agorachain.es"
-SSL_DIR="/etc/ssl/"
+# DOMAIN="agorachain.es"
+# SSL_DIR="/etc/ssl/"
 
-mkdir -p $SSL_DIR
+# mkdir -p $SSL_DIR
 
-openssl req -x509 -nodes -days 365 \
-    -newkey rsa:2048 \
-    -keyout "$SSL_DIR/private/privkey.pem" \
-    -out "$SSL_DIR/certs/fullchain.pem" \
-    -subj "/CN=$DOMAIN"
+# openssl req -x509 -nodes -days 365 \
+#     -newkey rsa:2048 \
+#     -keyout "$SSL_DIR/private/privkey.pem" \
+#     -out "$SSL_DIR/certs/fullchain.pem" \
+#     -subj "/CN=$DOMAIN"
 
-exec "$@"
+# exec "$@"
