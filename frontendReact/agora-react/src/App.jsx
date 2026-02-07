363c5b21ec499b4a4331b091f4c2e7d4c1e1b6b2
Oliver <olivergamboamesa0850@gmail.com>
2026-02-07 23:24:51 +0100

    refactor: extract reusable SweetAlert toast and popup styles

--- frontendReact/agora-react/src/App.jsx
+++ frontendReact/agora-react/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./services/alerts.css";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Index from "./pages/index/index";
 import Home from "./pages/Home/Home";
---
+++ frontendReact/agora-react/src/services/alerts.js
@@ -0,0 +1,28 @@
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+    toast: true,
+    showConfirmButton: false,
+    position: 'bottom-end',
+    timer: 2000,
+    timerProgressBar: true,
+    width: '200px',
+    padding: '0.5em',
+});
+  
+const Popup = Swal.mixin({
+    toast: false,
+    confirmButtonColor: "#3d0091",
+    allowOutsideClick: true,
+    allowEscapeKey: true,
+    customClass: {
+      popup: 'custom-popup',
+      container: "custom-backdrop"
+    }
+});
+
+export const popupError = (title) =>
+  Popup.fire({ icon: "error", title });
+
+export const toastSuccess = (title) =>
+  Toast.fire({ icon: "success", title });
