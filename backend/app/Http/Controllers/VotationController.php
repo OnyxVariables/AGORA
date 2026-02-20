8208f0e46cab7d687f5d1ccf302e30d9f4c6f1d1
Oliver <silver.gw.03@gmail.com>
2026-02-20 15:10:29 +0100

    Fix auth:samctum for API of admin

--- backend/app/Http/Controllers/VotationController.php
+++ backend/app/Http/Controllers/VotationController.php
@@ -10,12 +10,20 @@ class VotationController extends Controller
     // READ
     public function index()
     {
+        if (Auth::user()->roleId !== 1) {
+            return response()->json(['error' => 'No tienes permiso'], 403);
+        }
+        
         return response()->json(Votation::all(), 200, [], JSON_UNESCAPED_UNICODE);
     }
 
     // CREATE
     public function store(Request $request)
     {
+        if (Auth::user()->roleId !== 1) {
+            return response()->json(['error' => 'No tienes permiso'], 403);
+        }
+
         $data = $request->validate([
             'title' => 'required|string|max:100',
             'description' => 'nullable|string',
@@ -26,19 +34,19 @@ class VotationController extends Controller
 
         $fakeHash = hash('sha256', uniqid());
 
-    // INSERTO bloque de prueba en la tabla 'block' para que la base de datos no se queje de la Foreign Key
-    //En el siguiente sprint, la blockchain genera los bloques y se guardan en block en la BBDD donde se referencia aquí
-    \DB::table('block')->insertOrIgnore([
-        'hash' => $fakeHash,
-        'blockNumber' => rand(1, 1000),
-        'previousHash' => '0',
-        'transactions' => 0,
-        'isValid' => 1,
-        'createdAt' => now()
-    ]);
+        // INSERTO bloque de prueba en la tabla 'block' para que la base de datos no se queje de la Foreign Key
+        //En el siguiente sprint, la blockchain genera los bloques y se guardan en block en la BBDD donde se referencia aquí
+        \DB::table('block')->insertOrIgnore([
+            'hash' => $fakeHash,
+            'blockNumber' => rand(1, 1000),
+            'previousHash' => '0',
+            'transactions' => 0,
+            'isValid' => 1,
+            'createdAt' => now()
+        ]);
 
-    $data['startBlockHash'] = $fakeHash;
-    $data['endBlockHash'] = $fakeHash;
+        $data['startBlockHash'] = $fakeHash;
+        $data['endBlockHash'] = $fakeHash;
 
         $votation = Votation::create($data);
 
@@ -48,6 +56,10 @@ class VotationController extends Controller
     // UPDATE
     public function update(Request $request, $id)
     {
+        if (Auth::user()->roleId !== 1) {
+            return response()->json(['error' => 'No tienes permiso'], 403);
+        }
+
         $votation = Votation::findOrFail($id);
 
         $votation->update($request->all());
@@ -58,6 +70,10 @@ class VotationController extends Controller
     // DELETE
     public function destroy($id)
     {
+        if (Auth::user()->roleId !== 1) {
+            return response()->json(['error' => 'No tienes permiso'], 403);
+        }
+
         Votation::destroy($id);
         return response()->json(null, 204);
     }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -12,17 +12,13 @@ Route::get('/login-cert', [CertAuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [UserController::class, 'me']);
     Route::post('/nickname', [UserController::class, 'setNickname']);
+    Route::post('/vote', [VoteController::class, 'send']);
 });
 
 // Ruta protegida por Sanctum donde solo puede acceder usuario autenticado
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware('auth:sanctum', 'role:1')->group(function () {
     Route::get('/votations', [VotationController::class, 'index']);
     Route::post('/votations', [VotationController::class, 'store']);
     Route::put('/votations/{id}', [VotationController::class, 'update']);
     Route::delete('/votations/{id}', [VotationController::class, 'destroy']);
-});
-
-// Ruta protegida por Sanctum donde solo puede acceder usuario autenticado
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/vote', [VoteController::class, 'send']);
 });
