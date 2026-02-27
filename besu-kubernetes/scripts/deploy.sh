b011391b29f65e27105a18192efd5a50e44494d5
Ubuntu <ubuntu@ip-172-31-90-224.ec2.internal>
2026-02-27 22:03:11 +0000

    Fix: resolution of DNS from K8s and throws BESU

--- besu-kubernetes/scripts/deploy.sh
+++ besu-kubernetes/scripts/deploy.sh
@@ -4,10 +4,9 @@ echo "Desplegando Red QBFT en Kubernetes"
 # Creo los recursos en el orden correcto
 kubectl create configmap besu-genesis-config \
   --from-file=../network-config/genesis.json \
-  --from-file=../network-config/static-nodes.json \
   --dry-run=client -o yaml | kubectl apply -f -
 kubectl apply -f ../k8s-manifests/secrets.yaml
 kubectl apply -f ../k8s-manifests/service.yaml
 kubectl apply -f ../k8s-manifests/statefulset.yaml
 
-echo "Despliegue iniciado. Para verificar: watch kubectl get pods"
+echo "Despliegue iniciado. Para verificar: kubectl get pods"
