70fd9287caec9f4af64d1d5f1e800b5f8688231b
Ubuntu <ubuntu@ip-172-31-90-224.ec2.internal>
2026-02-28 17:54:40 +0000

    Checked url for Prometheus

--- besu-kubernetes/scripts/deploy.sh
+++ besu-kubernetes/scripts/deploy.sh
@@ -11,11 +11,11 @@ kubectl create configmap besu-genesis-config \
 kubectl apply -n besu -f ../k8s-manifests/secrets.yaml
 kubectl apply -n besu -f ../k8s-manifests/service.yaml
 kubectl apply -n besu -f ../k8s-manifests/statefulset.yaml
-echo "Despliegue iniciado. Para verificar: kubectl get pods"
+echo "Despliegue iniciado. Para verificar: kubectl get pods -n besu"
 
 # Wait for the pod to be ready before deploying monitoring
 echo "Instalando Prometheus y Grafana con Helm"
-helm repo add prometheus-community https://prometheus-community.github.io
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
   --namespace besu \
@@ -25,4 +25,4 @@ helm upgrade --install monitoring prometheus-community/kube-prometheus-stack \
 
 kubectl apply -f ../k8s-manifests/besu-servicemonitor.yaml
 
-echo "Success Deploy. Accede a Grafana en el puerto 3000"
+echo "Success Deploy. Accede a Grafana en el puerto 30000"
