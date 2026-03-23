d93d10c7afb10bdc5db9d0902d5b33ab670f2d24
Oliver <silver.gw.03@gmail.com>
2026-03-23 21:23:39 +0100

    feat: implement blockchain event listeners for voting contract

--- votations/src/main/java/com/agora/votations/config/Web3Config.java
+++ votations/src/main/java/com/agora/votations/config/Web3Config.java
@@ -10,8 +10,6 @@ public class Web3Config {
 
     @Bean
     public Web3j web3j() {
-        String rpcUrl = "http://localhost:8545";
-
-        return Web3j.build(new HttpService(rpcUrl));
+        return Web3j.build(new HttpService("http://localhost:8545"));
     }
 }
