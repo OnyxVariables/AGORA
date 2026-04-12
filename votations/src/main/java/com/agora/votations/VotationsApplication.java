8d0475a89a1a93c24257f1efbc4028004179eebe
Oliver <silver.gw.03@gmail.com>
2026-04-13 00:23:44 +0200

    fix: SpringBoot listener for pass status to ACTIVE and many debug

--- votations/src/main/java/com/agora/votations/VotationsApplication.java
+++ votations/src/main/java/com/agora/votations/VotationsApplication.java
@@ -1,13 +1,25 @@
 package com.agora.votations;
 
+import com.agora.votations.service.BlockchainListenerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class VotationsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VotationsApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner verifyBeans(BlockchainListenerService listener) {
+		return args -> {
+			log.info("VERIFICACION: BlockchainListenerService bean exists: {}", listener != null);
+		};
 	}
 
 }
